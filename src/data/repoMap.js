5b93d08fdd4696c3802bc4e265d09a95f2aa3766
Andre Torres <andreh.torres@hotmail.com>
2025-04-08 09:01:52 -0300

    fix: js data inject

--- src/data/repoMap.js
+++ src/data/repoMap.js
@@ -1,8 +1,8 @@
-import churnPolarisLLM from './repos/code_churn_polaris-llm.js';
-import reworkPolarisLLM from './repos/rework_rate_polaris-llm.js';
+import churnPolarisLLM from 'src/data/repos/code_churn_polaris-llm.js';
+import reworkPolarisLLM from 'src/data/repos/rework_rate_polaris-llm.js';
 
-import churnPolarisPythonApi from './repos/code_churn_polaris-python-api.js';
-import reworkPolarisPythonApi from './repos/rework_rate_polaris-python-api.js';
+import churnPolarisPythonApi from 'src/data/repos/code_churn_polaris-python-api.js';
+import reworkPolarisPythonApi from 'src/data/repos/rework_rate_polaris-python-api.js';
 
 export const repoMap = {
     'polaris-python-api': {
